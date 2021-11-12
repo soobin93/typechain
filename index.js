5dc823dbe471b7f3b0c5880aa35fd0ff14b16640
Soobin <ssbin93@gmail.com>
2021-11-12 22:19:27 +1000

    Playing around with Typescript's class

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 "use strict";
+// interface Human {
+//     name: string,
+//     age: number,
+//     gender: string
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Soobin Son",
-    age: 27,
-    gender: "male"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const soobin = new Human('Soobin', 27, 'male');
 const sayHi = ({ name, age, gender }) => {
     console.log(`Hello ${name}, you are ${age} years old, and you are a ${gender}`);
 };
-sayHi(person);
+sayHi(soobin);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,19 +1,27 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+// interface Human {
+//     name: string,
+//     age: number,
+//     gender: string
+// }
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Soobin Son",
-    age: 27,
-    gender: "male"
-};
+const soobin = new Human('Soobin', 27, 'male');
 
 const sayHi = ({ name, age, gender }: Human) => {
     console.log(`Hello ${name}, you are ${age} years old, and you are a ${gender}`);
 };
 
-sayHi(person);
+sayHi(soobin);
 
 export {};
